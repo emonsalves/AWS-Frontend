9fdfe61c66138b4d6a8e43aef7c4bc741f82a838
Emonsalves <ingeniero.monsalves@gmail.com>
2023-09-24 22:19:46 -0300

    add : animated css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,10 +16,10 @@ const Home = () => {
       <div className="firefly"></div>
       <div className="firefly"></div>
       <div className="firefly"></div>
-      <h1 className="text-5xl md:text-7xl text-white font-bold mb-8 z-10">
+      <h1 className="text-5xl md:text-7xl text-white font-bold mb-8 z-10 animate-fade-up animate-once">
         Coming Soon
       </h1>
-      <p className="text-white text-xl md:text-2xl text-center">
+      <p className="text-white text-xl md:text-2xl text-center animate-fade-up animate-once">
         We re working hard to bring you something amazing. Stay tuned!
       </p>
     </div>
