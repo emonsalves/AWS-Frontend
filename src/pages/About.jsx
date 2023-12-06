822a34053b283938523df5cdbaa66e65a07f23d8
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2023-12-06 00:22:57 -0300

    Add About page with TabsRender component

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import TabsRender from "../components/tabs/tabAbout";
+
+const About = () => {
+    return (
+        <div className="min-h-full bg-gray-900 flex flex-col items-center text-white">
+            <h1>About Page</h1>
+            <TabsRender />
+        </div>
+    );
+};
+
+export default About;
