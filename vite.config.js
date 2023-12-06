97e673bc79143cdc55e31082f3e110fa1a7b74e8
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2023-12-05 21:46:28 -0300

    Update code formatting and configuration files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
 
-server: {
-    port: 3500, // Puerto de salida
-  },
-})
+    server: {
+        port: 3500, // Puerto de salida
+    },
+});
