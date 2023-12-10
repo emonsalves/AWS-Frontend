fde7a63dad8700a15ec59e6a1d16a5b9b418c367
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2023-12-10 01:48:33 -0300

    Fix navigation menu and update About page layout

--- src/components/containers/ContainerAbout.jsx
+++ src/components/containers/ContainerAbout.jsx
@@ -2,23 +2,21 @@ const ContainerAbout = () => {
     return (
         <>
             <div className="flex flex-wrap border border-gray-400 shadow-custom-dark rounded-lg flex-col bg-gray-100 w-[80%] animate-fade-up animate-once mt-4 mb-10">
-                <h1 className="text-black my-5 text-xl lg:text-3xl animate-fade animate-once text-center">
-                    <strong> ¡Bienvenido a MNSLVS Solutions! </strong>
-                </h1>
+                <h1 className="text-black my-5 font-bold text-3xl animate-fade animate-once text-center">Acerca de Nosotros</h1>
                 <hr />
                 <div className="flex flex-col lg:flex-row">
                     <div className="lg:w-1/2">
                         <div className="card-title">
-                            <h2 className="text-black my-5 text-lg lg:text-2xl animate-fade animate-once text-center m-5">
+                            <h2 className="text-black font-semibold my-5 text-2xl animate-fade animate-once text-center m-5">
                                 Impulsando el Éxito de las PYMES a través de la Tecnología
                             </h2>
                         </div>
                         <div className="card-image overflow-hidden flex justify-center">
-                            <img className="m-5" src="https://i.imgur.com/5ZQZQ8i.png" alt="MNSLVS Solutions" width="85%" />
+                            <img className="m-5 rounded-lg drop-shadow-lg" src="./imagenDev.png" alt="MNSLVS Solutions" width="85%" />
                         </div>
                     </div>
                     <div className="card-text lg:w-1/2">
-                        <p className="text-black text-md animate-fade-left animate-once m-10">
+                        <p className="text-black text-md animate-fade-left animate-once m-10 text-justify">
                             Somos tu aliado estratégico en Tecnología, especialmente diseñado para potenciar el éxito de las pequeñas y medianas
                             empresas (PYMES)!
                             <br /> <br /> En MNSLVS, entendemos las necesidades únicas de las PYMES y nos comprometemos a ofrecer soluciones
@@ -26,57 +24,42 @@ const ContainerAbout = () => {
                         </p>
                     </div>
                 </div>
-            </div>
-            <div className="flex flex-wrap border border-gray-400 shadow-custom-dark rounded-lg flex-col bg-gray-100 w-[80%] animate-fade-up animate-once mt-4 mb-10">
-                <h2 className="text-black my-5 text-lg lg:text-2xl animate-fade animate-once text-left m-5">Nuestra Historia</h2>
 
-                <p className="text-black text-md animate-fade-left animate-once m-10">
-                    MNSLVS Solutions fue fundada en 2021 por un grupo de amigos y colegas que se conocieron en la universidad. Después de graduarse,
-                    cada uno de nosotros se embarcó en diferentes caminos profesionales, pero siempre mantuvimos el contacto y compartimos el mismo
-                    sueño: crear una empresa que pudiera ayudar a las PYMES a aprovechar al máximo la tecnología.
-                    <br />
-                    <br />
-                    En 2021, decidimos unir fuerzas y hacer realidad nuestro sueño. Así nació MNSLVS Solutions.
-                </p>
+                <div className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col"></div>
+                </div>
             </div>
 
             <div className="flex flex-col md:flex-row border border-gray-400 shadow-custom-dark rounded-lg bg-gray-100 w-[80%] animate-fade-up animate-once mt-4 mb-10">
-                <div className="flex flex-col">
-                    <h2 className="text-black my-5 text-lg lg:text-2xl animate-fade animate-once text-left m-5">Nuestra Misión:</h2>
-
-                    <p className="text-black text-md animate-fade-left animate-once m-10">
+                <div className="flex flex-col md:w-1/2">
+                    <h2 className="text-black my-5 font-semibold text-2xl animate-fade animate-once text-center m-5">Nuestra Historia:</h2>
+                    <hr />
+                    <p className="text-black text-md animate-fade-left animate-once m-10 text-justify">
+                        MNSLVS Solutions fue fundada en 2021 por un grupo de amigos y colegas que se conocieron en la universidad. Después de
+                        graduarse, cada uno de nosotros se embarcó en diferentes caminos profesionales, pero siempre mantuvimos el contacto y
+                        compartimos el mismo sueño: crear una empresa que pudiera ayudar a las PYMES a aprovechar al máximo la tecnología.
+                        <br />
+                        <br />
+                        En 2021, decidimos unir fuerzas y hacer realidad nuestro sueño. Así nació MNSLVS Solutions.
+                    </p>
+                </div>
+                <div className="flex flex-col md:w-1/2">
+                    <h2 className="text-black my-5 font-semibold text-2xl animate-fade animate-once text-center m-5">Nuestra Misión:</h2>
+                    <hr />
+                    <p className="text-black text-md animate-fade-left animate-once m-10 text-justify">
                         En MNSLVS Solutions, nuestra misión es simple pero crucial: empoderar a las PYMES a través de la tecnología. <br /> <br />
                         Buscamos proporcionar soluciones que no solo mejoren la eficiencia operativa, sino que también impulsen el crecimiento
                         sostenible de nuestros clientes.
                     </p>
                 </div>
-                <div className="flex flex-col">
-                    <h2 className="text-black my-5 text-lg lg:text-2xl animate-fade animate-once text-left m-5">Nuestro Compromiso con las PYMES:</h2>
-
-                    <p className="text-black text-md animate-fade-left animate-once m-10">
-                        Comprendemos los desafíos que enfrentan las PYMES y nos enorgullece ser un socio confiable en su viaje hacia el éxito. <br />
-                        <br />
-                        Desde la optimización de procesos hasta la implementación de soluciones personalizadas, estamos aquí para hacer que la
-                        tecnología trabaje para ti, sin importar el tamaño de tu empresa.
-                    </p>
-                </div>
             </div>
 
             <div className="flex flex-wrap border border-gray-400 shadow-custom-dark rounded-lg flex-col bg-gray-100 w-[80%] animate-fade-up animate-once mt-4 mb-10">
-                <h2 className="text-black my-5 text-lg lg:text-2xl animate-fade animate-once text-left m-5">Nuestro Enfoque Personalizado:</h2>
+                <h2 className="text-black my-5 font-semibold text-2xl animate-fade animate-once text-center m-5">
+                    Servicios Especializados para PYMES:
+                </h2>
 
-                <p className="text-black text-md animate-fade-left animate-once m-5">
-                    En MNSLVS Solutions, reconocemos que cada PYME es única. Nos tomamos el tiempo para comprender tus metas, desafíos y aspiraciones
-                    específicas. <br />
-                    <br />
-                    Esto nos permite diseñar soluciones tecnológicas adaptadas a tus necesidades y presupuesto.
-                </p>
-            </div>
-
-            <div className="flex flex-wrap border border-gray-400 shadow-custom-dark rounded-lg flex-col bg-gray-100 w-[80%] animate-fade-up animate-once mt-4 mb-10">
-                <h2 className="text-black my-5 text-lg lg:text-2xl animate-fade animate-once text-left m-5">Servicios Especializados para PYMES:</h2>
-
-                <p className="text-black text-md animate-fade-left animate-once m-10">
+                <p className="text-black text-md animate-fade-left animate-once m-10 text-justify">
                     Desarrollo Web Personalizado: Creamos sitios web atractivos y funcionales que se adaptan a las necesidades específicas de tu PYME.
                     <br />
                     <br />
@@ -88,24 +71,11 @@ const ContainerAbout = () => {
             </div>
 
             <div className="flex flex-wrap border border-gray-400 shadow-custom-dark rounded-lg flex-col bg-gray-100 w-[80%] animate-fade-up animate-once mt-4 mb-10">
-                <h2 className="text-black my-5 text-lg lg:text-2xl animate-fade animate-once text-left m-5">Valores que Nos Definen:</h2>
-
-                <p className="text-black text-md animate-fade-left animate-once m-10">
-                    Accesibilidad: Creemos en hacer que la tecnología sea accesible para todas las PYMES, independientemente de su presupuesto.
-                    <br />
-                    <br />
-                    Confianza: Construimos relaciones sólidas basadas en la confianza y la transparencia. <br />
-                    <br />
-                    Innovación Práctica: Buscamos soluciones innovadoras que sean prácticas y tangibles para las PYMES.
-                </p>
-            </div>
-
-            <div className="flex flex-wrap border border-gray-400 shadow-custom-dark rounded-lg flex-col bg-gray-100 w-[80%] animate-fade-up animate-once mt-4 mb-10">
-                <h2 className="text-black my-5 text-lg lg:text-2xl animate-fade animate-once text-left m-5">
+                <h2 className="text-black my-5 font-semibold text-2xl animate-fade animate-once text-center m-5">
                     Compromiso con la Calidad y la Eficiencia:
                 </h2>
 
-                <p className="text-black text-md animate-fade-left animate-once m-10">
+                <p className="text-black text-md animate-fade-left animate-once m-10 text-justify">
                     La calidad y la eficiencia son fundamentales en cada proyecto que emprendemos. Nos esforzamos por ofrecer soluciones que no solo
                     cumplen con tus expectativas, sino que también superan cualquier desafío que tu PYME pueda enfrentar. <br />
                     <br />
